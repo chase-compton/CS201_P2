abe8012f18ff7748fabea6fc90555e6f75ae1a7c
chase-compton <cscompton1@crimson.ua.edu>
2022-10-10 13:24:49 -0500

    yuh

--- RBTree.cpp
+++ RBTree.cpp
@@ -5,77 +5,31 @@ using namespace std;
 template <class typeOne, class typeTwo>
 class Node
 {
+protected:
     typeOne key;
     typeTwo value;
     int color;
-    Node left;
-    Node right;
-    Node parent;
+    Node *left;
+    Node *right;
+    Node *parent;
 
 public:
-    
+    Node()
+    {
+        key = 1;
+        value = "Default Node";
+        color = 1;
+        left = nullptr;
+        right = nullptr;
+        parent = nullptr;
+    }
     void print()
     {
         cout << "Key: " << key << " Value: " << value << " Color: " << color << endl;
     }
 };
 
-template <class type>
-class CircularDynamicArray
-{
-protected:
-    int size;
-    int cap;
-    int front;
-    type *array;
-    type outOfbounds;
+int main(){
+    Node 
 
-public:
-    CircularDynamicArray()
-    {
-        size = 0;
-        cap = 2;
-        array = new type[cap];
-        front = 0;
-    }
-
-    CircularDynamicArray(int s)
-    {
-        size = s;
-        cap = s;
-        array = new type[cap];
-        front = 0;
-    }
-
-    CircularDynamicArray(const CircularDynamicArray &rhs)
-    {
-        size = rhs.size;
-        cap = rhs.cap;
-        array = new type[cap];
-        for (int i = 0; i < size; i++)
-        {
-            array[i] = *(rhs.array + ((rhs.front + i) % cap));
-        }
-        front = 0;
-    }
-
-    CircularDynamicArray &operator=(const CircularDynamicArray &rhs)
-    {
-        size = rhs.size;
-        cap = rhs.cap;
-        type *tempArray = new type[cap];
-        for (int i = 0; i < size; i++)
-        {
-            tempArray[i] = *(rhs.array + ((front + i) % cap));
-        }
-        delete[] array;
-        array = tempArray;
-        front = 0;
-        return *this;
-    }
-
-    ~CircularDynamicArray()
-    {
-        delete[] array;
-    }
-};
+}
