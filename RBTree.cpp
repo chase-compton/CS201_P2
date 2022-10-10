f764dae45c347422a13e2e09078aa3e6cb30012e
chase-compton <cscompton1@crimson.ua.edu>
2022-10-10 09:38:09 -0500

    first

---
+++ RBTree.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+template <class type>
+class CircularDynamicArray
+{
+protected:
+    int size;
+    int cap;
+    int front;
+    type *array;
+    type outOfbounds;
+
+public:
+    CircularDynamicArray()
+    {
+        size = 0;
+        cap = 2;
+        array = new type[cap];
+        front = 0;
+    }
+
+    CircularDynamicArray(int s)
+    {
+        size = s;
+        cap = s;
+        array = new type[cap];
+        front = 0;
+    }
+
+    CircularDynamicArray(const CircularDynamicArray &rhs)
+    {
+        size = rhs.size;
+        cap = rhs.cap;
+        array = new type[cap];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = *(rhs.array + ((rhs.front + i) % cap));
+        }
+        front = 0;
+    }
+
+    CircularDynamicArray &operator=(const CircularDynamicArray &rhs)
+    {
+        size = rhs.size;
+        cap = rhs.cap;
+        type *tempArray = new type[cap];
+        for (int i = 0; i < size; i++)
+        {
+            tempArray[i] = *(rhs.array + ((front + i) % cap));
+        }
+        delete[] array;
+        array = tempArray;
+        front = 0;
+        return *this;
+    }
+
+    ~CircularDynamicArray()
+    {
+        delete[] array;
+    }
+};
